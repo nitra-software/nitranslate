95ba47ac135119a433248c1c226958c3574e6842
tecnologico005 <studenttecno@outlook.es>
2026-09-13 15:29:22 +0200

    Experimental handler for translations

---
+++ translations-handler.py
@@ -0,0 +1,15 @@
+import gettext
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LOCALE_DIR = os.path.join(BASE_DIR, 'locale')
+DOMAIN = 'messages'
+_active_translation = gettext.NullTranslations()
+
+def english():
+	global _active_translation
+	try:
+		_active_translation = gettext.translation(domain=DOMAIN, localedir=LOCALE_DIR, languages=['en'])
+	except FileNotFoundError:
+		_active_translation = gettext.NullTranslations()
+	_active_translation.install()
+	return _active_translation.gettext
