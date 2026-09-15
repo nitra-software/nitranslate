f98e3601745640170aeb228d78c2bc44d731e330
tecnologico005 <studenttecno@outlook.es>
2026-09-15 21:51:55 +0200

    Fixed a bug

--- boot.py
+++ boot.py
@@ -1,11 +1,10 @@
 import gettext
 gettext.install("lang")
 from pathlib import Path
-import main_window
 import translations_handler
 import no_language_selected
 import wx
-import translations_handler
+
 app=wx.App()
 if Path("default_language.txt").exists():
 	with open("default_language.txt", "r") as archivo:
@@ -14,6 +13,7 @@ if Path("default_language.txt").exists():
 			ventana = no_language_selected.Ventana()
 		else:
 			translations_handler.set_language(content)
+			import main_window
 			ventana = main_window.Ventana()
 else:
 	with open("default_language.txt", "w") as creado:
