66b3576ee21082a4e0dd0317d005066b4fbbc227
tecnologico005 <studenttecno@outlook.es>
2026-09-15 17:47:02 +0200

    Fixed a bug that prevents nitranslate to execute if default-language.txt doesn't exist

--- boot.py
+++ boot.py
@@ -1,17 +1,21 @@
 import gettext
 gettext.install("lang")
+from pathlib import Path
 import main_window
 import translations_handler
 import no_language_selected
 import wx
 import translations_handler
 app=wx.App()
-
-with open("default_language.txt", "r") as archivo:
-	content=archivo.read()
-	if content == "":
+if Path("default_language.txt").exists():
+	with open("default_language.txt", "r") as archivo:
+		content=archivo.read()
+		if content == "":
+			ventana = no_language_selected.Ventana()
+		else:
+			translations_handler.set_language(content)
+			ventana = main_window.Ventana()
+else:
+	with open("default_language.txt", "w") as creado:
 		ventana = no_language_selected.Ventana()
-	else:
-		translations_handler.set_language(content)
-		ventana = main_window.Ventana()
 app.MainLoop()
